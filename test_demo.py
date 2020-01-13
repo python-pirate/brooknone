49f517cd718c0bc6ba83388ea427d65259da8e2f
brooknone <brook.none@qq.com>
2020-01-13 20:30:08 +0800

    max/min value
    Resolve #1

--- test_demo.py
+++ test_demo.py
@@ -2,13 +2,14 @@ from demo import *
 import random
 
 def test_max_min():
-    arr = []
-    
-    i = 0
-    while i < 10:
-        arr.append(random.randint(10, 99))
-        i += 1
+    for _ in range(10000):
+        i = 0
+        arr = []
+        
+        while i < 10:
+            arr.append(random.randint(10, 99))
+            i += 1
 
-    bmax = max_min(arr)
-    for x in arr:
-        assert(bmax >= x)
+        bmax = max_min(arr)
+        for x in arr:
+            assert(bmax >= x)
