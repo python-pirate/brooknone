a151152306d7dbd18aab9216ed0adbbdc7f0ee5e
brooknone <brook.none@qq.com>
2020-01-13 20:09:53 +0800

    max/min value

--- demo.py
+++ demo.py
@@ -1,2 +1,11 @@
-def foo():
-    return False
+import random
+
+def max_min(ar):
+    y = 0
+    bmax = ar[0]
+    while y < len(ar):
+        if bmax < ar[y]:
+            bmax = ar[y]
+        y += 1
+
+    return bmax
--- test_demo.py
+++ test_demo.py
@@ -3,10 +3,12 @@ import random
 
 def test_max_min():
     arr = []
-    for a in arr:
+    
+    i = 0
+    while i < 10:
         arr.append(random.randint(10, 99))
+        i += 1
 
     bmax = max_min(arr)
     for x in arr:
         assert(bmax >= x)
-        
