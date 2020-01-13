0512809d486713e5f352da23839d3e086ce919bb
brooknone <brook.none@qq.com>
2020-01-13 17:52:01 +0800

    max/min value

--- test_demo.py
+++ test_demo.py
@@ -1,5 +1,11 @@
 from demo import *
+import random
 
-def test_foo():
-    res = foo()
-    assert(res == True)
+def test_max_min():
+    arr = []
+    for a in arr:
+        arr.append(random.randint(10, 99))
+
+    bmax = max_min(arr)
+    for x in arr:
+        assert(bmax >= x)
