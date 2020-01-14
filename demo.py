1eebc126fa1c112240d091437a2640fd31aa6df2
brooknone <brook.none@qq.com>
2020-01-14 10:31:27 +0800

    selection sort
    Resolve #2

--- demo.py
+++ demo.py
@@ -1,2 +1,14 @@
-def foo():
-    return False
+def select_sort(ar):
+    j = len(ar)
+    while j > 1:
+        i = 1
+        m = 0
+
+        while i < j:
+            if ar[m] < ar[i]:
+                m = i
+            i += 1
+
+        if ar[m] != ar[j - 1]:
+            ar[m], ar[j - 1] = ar[j - 1], ar[m]
+        j -= 1
--- test_demo.py
+++ test_demo.py
@@ -1,5 +1,13 @@
 from demo import *
+import random
 
-def test_foo():
-    res = foo()
-    assert(res == True)
+def test_select_sort():
+    for _ in range(10000):
+        i = 0
+        arr = [random.randint(10, 99) for _ in range(10)]
+
+        select_sort(arr)
+
+        while i < len(arr) - 1:
+            assert(arr[i] <= arr[i + 1])
+            i += 1
